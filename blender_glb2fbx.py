edf8b24519801c572db70d691f1b32fcda2625ad
daot@microsoft.com <daot@microsoft.com>
2019-01-17 11:51:09 -0800

    working

--- blender_glb2fbx.py
+++ blender_glb2fbx.py
@@ -1,6 +1,11 @@
 import sys
 import bpy
 
+#clean out the scene
+bpy.ops.object.select_all(action='SELECT')
+bpy.ops.object.delete()
+
+
 print ("%s: Input file: %s"%(sys.argv[0],sys.argv[-2]))
 print ("%s: Ouput file: %s"%(sys.argv[0],sys.argv[-1]))
 bpy.ops.import_scene.gltf(filepath=sys.argv[-2])
