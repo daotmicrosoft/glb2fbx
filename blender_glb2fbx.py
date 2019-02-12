b0e969c34e9c5f89db002eed85d295bf18ae1da7
daveotte <otte.dave@gmail.com>
2019-02-12 14:33:31 -0800

    removing debugging print statement.

--- blender_glb2fbx.py
+++ blender_glb2fbx.py
@@ -10,6 +10,5 @@ bpy.ops.object.delete()
 print ("%s: Input file: %s"%(sys.argv[0],sys.argv[-2]))
 print ("%s: Ouput file: %s"%(sys.argv[0],sys.argv[-1]))
 bpy.ops.import_scene.gltf(filepath=sys.argv[-2])
-print ("bob")
 bpy.ops.export_scene.fbx(filepath=sys.argv[-1], path_mode='COPY', embed_textures=True)
 
