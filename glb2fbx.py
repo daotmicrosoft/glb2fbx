929a39054387caaedf7239a48a1978367004b760
daveotte <otte.dave@gmail.com>
2019-02-12 16:27:15 -0800

    cleaned up, reorganized

--- glb2fbx.py
+++ glb2fbx.py
@@ -2,207 +2,160 @@
 # -*- coding: utf-8 -*-
 
 import docker
-import os
 import tempfile
 import shutil
+from pathlib import Path
+import argparse
+
 
 class Glb2Fbx:
 
     def __init__(
             self,
-            input_file,
-            output_file,
-            blender_script="blender_glb2fbx.py",
-            container_dir="/app/tmp",
-            image="daotmicrosoft/blender:2.8_ubuntu"):
-
-        self.input_file = input_file
-        self.output_file = output_file
-        self.blender_script = os.path.abspath(blender_script)
+            input_source_glb_file,
+            output_fbx_file,
+            input_blender_script_file="glb_to_fbx_blender.py",
+            input_container_dir="/app/tmp",
+            output_container_dir="/app/tmp1",
+            image="daotmicrosoft/blender:2.8_ubuntu",
+            debug=False):
+        """
+        Manage the data ingress and egress from the docker container. Files
+        prefixed with 'input_*_file' are consolidated into a created
+        'input_tmp_dir'. Then the container mounts the tmp_dir as
+        'input_container_dir'. The container will also mount the directory where
+        'output_*_file' will be written.
+        """
+        # blow away the tmp dirs when we're done?
+        self.debug = debug
+        try:
+            self.client = docker.from_env()
+            self.client.ping()
+        except:
+            raise Exception("Make sure docker desktop client is running. Cannot reach docker server.")
         self.image = image
-        self.container_dir = container_dir
 
-        self.client = docker.from_env()
+        # consolidate input files to 'input_tmp_dir'
+        #
+        # using resolve, since mac os returns a symbolic link,
+        # which docker cannot access.
+        self.input_tmp_dir = Path(tempfile.mkdtemp()).resolve().__str__()
+        self.input_container_dir = input_container_dir
+        self.output_container_dir = output_container_dir
 
-        self._remap_dir_to_container()
+        # map input/output_container_dir to input_tmp_dir, our directory of
+        self.volumes = dict()
+        self.volumes[self.input_tmp_dir] = {
+            'bind': self.input_container_dir, 'mode': 'rw'}
+        self.volumes[Path(output_fbx_file).absolute().parent.__str__()] = {
+            'bind': self.output_container_dir, 'mode': 'rw'}
 
-    def _remap_dir_to_container(self):
-        """
-        Replace the input and output dirs with
-        container_input/output_dir
-        """
-        # make a tmp dir, and link all files in that dir
-        self._tmp = tempfile.mkdtemp()
-        self._volume = {}
-        self._volume[self._tmp] = {'bind': self.container_dir, 'mode': 'rw'}
+        # check and consolodate the input files into tmp dir, and swap out paths
+        # for container input path (which are mapped to where the files are
+        # consolidated to)
+        self.check_input_file(input_source_glb_file, ['.glb', '.gltf'])
+        self.input_source_glb_file = self.containerize_file(
+            input_source_glb_file)
 
-        #link the input_file
-        input_file_linked_path = os.path.join(self._tmp, "input_file.glb")
-        print ("trying to link %s to %s"%(self.input_file, input_file_linked_path))
-        try:
-            os.symlink(self.input_file, input_file_linked_path)
-        except:
-            print ("Windows doesn't allow sym links. Copying file %s to %s"%(self.input_file, input_file_linked_path))
-            shutil.copyfile(self.input_file, input_file_linked_path)
+        self.check_input_file(input_blender_script_file, '.py')
+        self.input_blender_script_file = self.containerize_file(
+            input_blender_script_file)
 
+        # check and swap out path for container output path
+        self.check_output_file_dir(output_fbx_file, '.fbx')
+        self.returned_output_fbx_file = output_fbx_file
+        self.output_fbx_file = self.swap_directory_paths(
+            output_fbx_file, output_container_dir)
 
-        #link the directory the container writes to
-        output_dir_linked_path = os.path.join(self._tmp, "output_dir")
-        try:
-            os.symlink(os.path.dirname(self.output_file), output_dir_linked_path)
-            self.copy_output = False
-        except:
-            print ("Windows doesn't allow sym links. Will copy output out of container.")
-            output_dir_linked_path = self.container_dir
-            self.copy_output = True
-
-        #link the script
-        blender_script_linked_path = os.path.join(self._tmp, "script.py")
-        try:
-            os.symlink(self.blender_script, blender_script_linked_path)
-        except:
-            print ("Windows doesn't allow sym links. Copying file %s to %s"%(self.blender_script, blender_script_linked_path))
-            shutil.copyfile(self.blender_script, blender_script_linked_path)
-
-        #now each of the input files are symbolically one dir, which is mapped
-        #to the container
-
-        #next, we replace the local tmp path with the container path for each
-        #file
-        self.input_arg = unixify_path(os.path.join(self.container_dir, os.path.basename(input_file_linked_path)))
-        self.output_arg = unixify_path(os.path.join(output_dir_linked_path, os.path.basename(self.output_file)))
-        self.script_arg = unixify_path(os.path.join(self.container_dir, os.path.basename(blender_script_linked_path)))
-
-        
-
+    def containerize_file(self, source_file):
+        source_file = Path(source_file).absolute().__str__()
+        dest_file = Path(self.input_tmp_dir, Path(source_file).name).__str__()
+        print("Copying %s to %s" % (source_file, dest_file))
+        shutil.copyfile(source_file, dest_file)
+        return self.swap_directory_paths(source_file, self.input_container_dir)
 
     def __call__(self):
+        """
+        Do the business.
 
+        """
         cmd = "./blender_app/blender -b -P %s -- %s %s" % (
-            self.script_arg, self.input_arg, self.output_arg)
-
-
-        
+            self.input_blender_script_file,
+            self.input_source_glb_file,
+            self.output_fbx_file)
         print("Executing: %s in container." % cmd)
-
 
         _log = self.client.containers.run(
             self.image,
             cmd,
             stream=True,
             detach=False,
-            volumes=self._volume)
+            volumes=self.volumes)
 
         for line in _log:
-            print(format_blender_log(line))
+            print(line.decode('UTF-8'), end="")
 
-        print("Wrote: %s" % self.output_file)
-
-        if self.copy_output:
-            print ("----------> Copying %s to %s"%(self.output_arg, self.output_file))
-            src = os.path.join(self._tmp, os.path.basename(self.output_file))
-            shutil.copyfile(src,self.output_file)
-
-        print ("Cleaning up.")
-        shutil.rmtree(self._tmp)
-        return self.output_file
-
-
-def format_blender_log(line):
-    return str(line).replace(
-        "\\n",
-        "").replace(
-        "\\t",
-        "   ").replace(
-            "b'",
-            "").replace(
-                "'",
-        "")
-import argparse
-
-
-def ends_with_ext(_path, test_ext):
-    ext = os.path.splitext(_path)[1]
-    return ext.lower() == test_ext.lower()
-
-
-def _conform_path(_path):
-    _path = unixify_path(_path)
-    return os.path.abspath(_path)
-
-
-def check_input_flag(args):
-    args.input = os.path.abspath(args.input)
-    print ('CHECKINT %s'%(os.path.isfile(args.input)))
-
-    if not (os.path.isfile(args.input) and (ends_with_ext(args.input, ".glb") or \
-        ends_with_ext(args.input, ".gltf"))):
-        
-        raise ValueError("Must pass in a valid path to a .glb or .gltf file.")
-
-    args.input = _conform_path(args.input)
-    return args
-
-
-def check_output_flag(args):
-    input_file_name_no_ext = os.path.splitext(os.path.basename(args.input))[0]
-    
-    if args.output:
-        args.output = os.path.abspath(args.output)
-        _base_dir = os.path.dirname(args.output)
-        if not os.path.isdir(_base_dir):
-            raise ValueError(
-                "Must pass in a valid path to write the .fbx file.")
-
-        # may be a path or a path and file
-        ext = os.path.splitext(args.output)[1]
-        if ext:
-            # has a file extension
-            if ext.lower() != ".fbx":
-                raise ValueError("Output file extension must be .fbx")
-        # is a dir:
+        if self.debug:
+            print("Did you not get what you wanted? Here's the docker cmd to debug:")
+            print("docker run -itv %s:%s %s bash" % (self.input_tmp_dir.lower(), self.input_container_dir, self.image))
+            print("Run this inside the container:")
+            print(cmd)
         else:
-            if not os.path.isdir(args.output):
-                raise ValueError(
-                    "Must pass in a valid directory path to --output.")
+            print("Cleaning up.")
+            shutil.rmtree(self.input_tmp_dir)
 
-            # valid dir, so let's construct the output file name.
+        return self.returned_output_fbx_file
 
-            args.output = os.path.join(
-                args.output, input_file_name_no_ext+".fbx")
+    def check_input_file(self, input_file, extension):
+        _error = "%s either doesn't exist, or is wrong type." % input_file
+        if isinstance(extension, list):
+            for ext in extension:
+                if self.is_valid_file(input_file, ext):
+                    return True
+            raise ValueError(_error)
+        else:
+            if not self.is_valid_file(input_file, extension):
+                raise ValueError(_error)
 
-    else:
-        args.output = os.path.join(
-            os.path.dirname(
-                args.input), input_file_name_no_ext+".fbx")
+    def check_output_file_dir(self, _file, extension):
+        if not self.is_file_type(_file, extension):
+            raise ValueError("%s needs to end with: %s" % (_file, extension))
+        if not Path(_file).parent.exists():
+            raise ValueError("%s does not exist." % Path(_file).parent)
 
-    args.output = _conform_path(args.output)
+    def is_file_type(self, _file, extension):
+        return _file.lower().endswith(extension.lower())
 
-    return args
+    def is_valid_file(self, _file, extension):
+        return Path(_file).exists() and self.is_file_type(_file, extension)
 
-
-def _parse_args(args):
-    args = check_input_flag(args)
-    args = check_output_flag(args)
-    return args
-
-
-def unixify_path(path_name):
-    return path_name.replace("\\", "/")
+    def swap_directory_paths(self, source_file, dest_path):
+        """
+        Replace the path to 'source_file' with 'dest_path'
+        """
+        return Path(dest_path, Path(source_file).name)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description="Converts a .gltf or .glb file to .fbx. Uses blender 2.8 in a docker container to do the conversion.")
-    parser.add_argument("input",
-                        help="Path and file name to glb or gltf file to convert. Example: ../mydir/myfile.glb")
+        description="Converts a .gltf or .glb file to .fbx. Uses blender 2.8 in a docker container to do the "
+                    "conversion.")
 
-    parser.add_argument("--output", "-o", help="Full path and file name to glb or gltf file to convert. Example: ../mydir/myfile.fbx, or ../mydir")
-    args = _parse_args(parser.parse_args())
-    print (args)
+    parser.add_argument("input_glb_file",
+                        help="Path and file name to glb or gltf file to convert.")
 
-    #do the business
-    print ("Converting %s to %s"%(args.input, args.output))
-    Glb2Fbx(args.input, args.output)()
+    parser.add_argument("output_fbx_file",
+                        help="Full path and file name to glb or gltf file to convert.")
 
+    parser.add_argument(
+        '--debug',
+        dest='debug',
+        action='store_true',
+        help="Do not clean up tmp dir, and print out commands to debug in docker.")
+    parser.set_defaults(debug=False)
 
+    args = parser.parse_args()
+    Glb2Fbx(
+        args.input_glb_file,
+        args.output_fbx_file,
+        debug=args.debug)()
